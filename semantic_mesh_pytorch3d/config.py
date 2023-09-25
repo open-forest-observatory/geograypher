31a9e687f685b90c62140c90698fecbbfc8ca481
David Russell <djrussell@ucdavis.edu>
2023-09-25 20:41:07 +0000

    Update default file locations

--- semantic_mesh_pytorch3d/config.py
+++ semantic_mesh_pytorch3d/config.py
@@ -2,32 +2,29 @@ from pathlib import Path
 
 DATA_FOLDER = Path(Path(__file__).parent, "..", "data").resolve()
 
-DEFAULT_IMAGES_FOLDER = str(Path(DATA_FOLDER, "emerald_point_oblique_subset", "images"))
+DEFAULT_IMAGES_FOLDER = str(Path(DATA_FOLDER, "composite_20230520T0519", "images"))
 
 DEFAULT_LOCAL_MESH = str(
     Path(
         DATA_FOLDER,
-        "emerald_point_oblique_subset",
-        "exports",
-        "emerald_point_oblique_subset_20230925T1512_model_local.ply",
+        "composite_georef",
+        "composite_local_reset.ply",
     )
 )
 
 DEFAULT_GEOREF_MESH = str(
     Path(
         DATA_FOLDER,
-        "emerald_point_oblique_subset",
-        "exports",
-        "emerald_point_oblique_subset_20230925T1512_model_georeferenced.ply",
+        "composite_georef",
+        "composite_georef.ply",
     )
 )
 
 DEFAULT_CAM_FILE = str(
     Path(
         DATA_FOLDER,
-        "emerald_point_oblique_subset",
-        "exports",
-        "emerald_point_oblique_subset_20230925T1512_cameras.xml",
+        "composite_georef",
+        "composite_georef_cameras.xml",
     )
 )
 
