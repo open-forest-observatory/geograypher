4dbedb9ba22380573c9dd5408fe99ed4b3802605
David Russell <djrussell@ucdavis.edu>
2023-10-19 20:07:01 +0000

    Refactor mesh render entrypoint

--- multiview_prediction_toolkit/entrypoints/mesh_render.py
+++ multiview_prediction_toolkit/entrypoints/mesh_render.py
@@ -1,13 +1,15 @@
 import argparse
+import logging
 
 from multiview_prediction_toolkit.cameras import MetashapeCameraSet
 from multiview_prediction_toolkit.config import (
     DEFAULT_CAM_FILE,
+    DEFAULT_GEOPOLYGON_FILE,
     DEFAULT_IMAGES_FOLDER,
     DEFAULT_LOCAL_MESH,
     PATH_TYPE,
 )
-from multiview_prediction_toolkit.meshes import GeodataPhotogrammetryMesh
+from multiview_prediction_toolkit.meshes import TexturedPhotogrammetryMesh
 
 
 def parse_args():
@@ -22,67 +24,43 @@ def parse_args():
     parser.add_argument("--mesh-file", default=DEFAULT_LOCAL_MESH)
     parser.add_argument("--camera-file", default=DEFAULT_CAM_FILE)
     parser.add_argument("--image-folder", default=DEFAULT_IMAGES_FOLDER)
+    parser.add_argument("--mesh-downsample", type=float, default=1)
+    parser.add_argument("--vector-file", default=DEFAULT_GEOPOLYGON_FILE)
+    parser.add_argument("--vector-file-column", default="treeID")
+    parser.add_argument("--vis", action="store_true")
     parser.add_argument(
-        "--texture-type",
-        default=0,
-        type=int,
-        help="Enum for texture. 0: default texture, 1: dummy texture, 2: geofile texture",
+        "--screenshot-filename",
     )
     parser.add_argument(
-        "--run-vis", action="store_true", help="Run mesh and cameras visualization"
+        "--log-level",
+        default="info",
+        choices=list(logging._nameToLevel.keys()),
     )
-    parser.add_argument(
-        "--run-aggregation",
-        action="store_true",
-        help="Aggregate color from multiple viewpoints",
-    )
-    parser.add_argument(
-        "--run-render", action="store_true", help="Render out viewpoints"
-    )
+
     args = parser.parse_args()
     return args
 
 
-def main(
-    mesh_file: PATH_TYPE,
-    camera_file: PATH_TYPE,
-    image_folder: PATH_TYPE,
-    run_vis: bool,
-    run_aggregation: bool,
-    run_render: bool,
-    texture_type: int,
-):
-    """Entrypoint
-
-    Args:
-        mesh_file (PATH_TYPE): Path to mesh in local metashape coordinates, with .ply extension
-        camera_file (PATH_TYPE): Path to camera file from metashape with .xml extension
-        image_folder (PATH_TYPE): Path to image folder
-        run_vis (bool): Should the mesh be visualized
-        run_aggregation (bool): Should data from different viewpoints be aggregated onto the mesh
-        run_render (bool): Should images from the camera poses be rendered
-        texture_type (int): How should the mesh be textured
-    """
-    # Load this first because it's fast
-    camera_set = MetashapeCameraSet(camera_file, image_folder)
-    mesh = GeodataPhotogrammetryMesh(mesh_file)
-
-    if run_vis:
-        mesh.vis(screenshot_filename="vis/mesh_render.png")
-    if run_aggregation:
-        mesh.aggregate_viewpoints_pytorch3d(camera_set)
-    if run_render:
-        mesh.render_pytorch3d(camera_set, image_scale=0.25)
-
-
 if __name__ == "__main__":
     args = parse_args()
-    main(
-        args.mesh_file,
-        args.camera_file,
-        args.image_folder,
-        args.run_vis,
-        args.run_aggregation,
-        args.run_render,
-        args.texture_type,
+
+    logging.basicConfig(level=args.log_level.upper())
+
+    # Load the camera set
+    logging.info("Creating the camera set")
+    camera_set = MetashapeCameraSet(args.camera_file, args.image_folder)
+
+    # Load the mesh
+    logging.info("Loading the mesh")
+    mesh = TexturedPhotogrammetryMesh(
+        args.mesh_file, downsample_target=args.mesh_downsample
     )
+
+    mesh.get_values_for_verts_from_vector(
+        column_names=args.vector_file_column,
+        vector_file=args.vector_file,
+        set_vertex_IDs=True,
+    )
+
+    if args.vis or args.screenshot_filename is not None:
+        mesh.vis(screenshot_filename=args.screenshot_filename)
