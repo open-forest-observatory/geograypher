Allow rotation matrices to be slightly ne 1 and fix vertext->face conversion

multiview_prediction_toolkit/meshes/meshes.py
@@ -459,6 +459,11 @@ class TexturedPhotogrammetryMesh:
         if vert_IDs is None:
             raise ValueError("None")
 
+        vert_IDs = np.squeeze(vert_IDs)
+
+        if vert_IDs.ndim != 1:
+            raise ValueError(f"Can only perform conversion with one dimensional array but instead had {vert_IDs.ndim}")
+
         # Each row contains the IDs of each vertex
         IDs_per_face = vert_IDs[self.faces]
         # Now we need to "vote" for the best one
@@ -957,7 +962,7 @@ class TexturedPhotogrammetryMesh:
         is_rgb = (
             self.pyvista_mesh.active_scalars_name == "RGB"
             if vis_scalars is None
-            else (len(vis_scalars.shape[1]) > 1)
+            else (vis_scalars.shape[1] > 1)
         )
 
         # Add the mesh

multiview_prediction_toolkit/utils/parsing.py
@@ -16,8 +16,8 @@ def make_4x4_transform(rotation_str: str, translation_str: str, scale_str: str =
     rotation_np = np.fromstring(rotation_str, sep=" ")
     rotation_np = np.reshape(rotation_np, (3, 3))
 
-    if det := np.linalg.det(rotation_np) != 1.0:
-        raise ValueError(f"Inproper rotation matrix with determinant {det}")
+    if not np.isclose(np.linalg.det(rotation_np), 1.0, atol=1e-8, rtol=0):
+        raise ValueError(f"Inproper rotation matrix with determinant {np.linalg.det(rotation_np)}")
 
     translation_np = np.fromstring(translation_str, sep=" ")
     scale = float(scale_str)