198073454e2c0d1fa6759d77a74a5aee9c3f80a2
David Russell <djrussell@ucdavis.edu>
2023-11-07 21:38:46 +0000

    Update utils imports

--- multiview_prediction_toolkit/cameras/cameras.py
+++ multiview_prediction_toolkit/cameras/cameras.py
@@ -1,6 +1,6 @@
 from copy import deepcopy
-from typing import Tuple
 from pathlib import Path
+from typing import Tuple
 
 import geopandas as gpd
 import numpy as np
@@ -16,8 +16,8 @@ from skimage.transform import resize
 from tqdm import tqdm
 
 from multiview_prediction_toolkit.config import PATH_TYPE
+from multiview_prediction_toolkit.utils.geospatial import get_projected_CRS
 from multiview_prediction_toolkit.utils.image import get_GPS_exif
-from multiview_prediction_toolkit.utils.utils import get_projected_CRS
 
 
 class PhotogrammetryCamera:
