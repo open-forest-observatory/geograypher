a6f72ef0af1a5e3d31d7549d42bd9148687c2238
David Russell <djrussell@ucdavis.edu>
2023-12-04 15:06:15 +0000

    Add a new tabular segmentor

--- multiview_mapping_toolkit/segmentation/derived_segmentors.py
+++ multiview_mapping_toolkit/segmentation/derived_segmentors.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import numpy as np
+import pandas as pd
 from imageio import imread
 from skimage.transform import resize
 
@@ -42,3 +43,97 @@ class LookUpSegmentor(Segmentor):
             resized_image, num_classes=self.num_classes
         )
         return one_hot_image
+
+
+class TabularRectangleSegmentor(Segmentor):
+    def __init__(
+        self,
+        pred_folder,
+        image_folder,
+        image_shape=(4008, 6016),
+        label_key="label",
+        image_path_key="image_path",
+        imin_key="ymin",
+        imax_key="ymax",
+        jmin_key="xmin",
+        jmax_key="xmax",
+        predfile_extension="csv",
+    ):
+        self.pred_folder = pred_folder
+        self.image_folder = image_folder
+        self.image_shape = image_shape
+
+        self.label_key = label_key
+        self.image_path_key = image_path_key
+        self.imin_key = imin_key
+        self.imax_key = imax_key
+        self.jmin_key = jmin_key
+        self.jmax_key = jmax_key
+
+        self.predfile_extension = predfile_extension
+
+        files = sorted(Path(self.pred_folder).glob("*" + self.predfile_extension))
+        dfs = [pd.read_csv(f) for f in files]
+
+        self.labels_df = pd.concat(dfs)
+        self.grouped_labels_df = self.labels_df.groupby(by=self.image_path_key)
+        self.image_names = list(self.grouped_labels_df.groups.keys())
+        self.class_names = np.unique(self.labels_df[self.label_key]).tolist()
+        self.num_classes = len(self.class_names)
+
+    def segment_image(self, image, filename, image_scale, vis=False):
+        label_image = np.zeros(
+            self.image_shape + (len(self.class_names),), dtype=np.uint8
+        )
+
+        name = filename.name
+        if name in self.image_names:
+            df = self.grouped_labels_df.get_group(name)
+        # Return an all-zero segmentation image
+        else:
+            return label_image
+
+        for _, row in df.iterrows():
+            label = row[self.label_key]
+            label_ind = self.class_names.index(label)
+            label_image[
+                int(row[self.imin_key]) : int(row[self.imax_key]),
+                int(row[self.jmin_key]) : int(row[self.jmax_key]),
+                label_ind,
+            ] = 1
+
+        if vis:
+            index_label = np.argmax(label_image, axis=2).astype(float)
+            zeros_mask = np.sum(label_image, axis=2) == 0
+            index_label[zeros_mask] = np.nan
+            plt.imshow(index_label, vmin=0, vmax=10, cmap="tab10")
+            plt.show()
+
+        return label_image
+
+    def get_detection_centers(self, filename):
+        """_summary_
+
+        Args:
+            filename (_type_): _description_
+
+        Returns:
+            _type_: (n,2) array for (i,j) centers for each detection
+        """
+        if filename not in self.image_names:
+            # Empty array of detection centers
+            return np.zeros((0, 2))
+
+        # Extract the corresponding dataframe
+        df = self.grouped_labels_df.get_group(filename)
+
+        # Extract the columns for the bounding box corners
+        imin = df[self.imin_key].to_numpy()
+        imax = df[self.imax_key].to_numpy()
+
+        jmin = df[self.jmin_key].to_numpy()
+        jmax = df[self.jmax_key].to_numpy()
+
+        # Average the left-right, top-bottom pairs
+        centers = np.vstack([(imin + imax) / 2, (jmin + jmax) / 2]).T
+        return centers
