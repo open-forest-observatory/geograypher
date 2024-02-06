506aad04b2934a632e21d7103506e58c8ebc9ea2
David Russell <djrussell@ucdavis.edu>
2024-02-06 20:18:19 +0000

    Make showing segmentation labels more flexible

--- multiview_mapping_toolkit/utils/visualization.py
+++ multiview_mapping_toolkit/utils/visualization.py
@@ -3,23 +3,40 @@ import numpy as np
 from imageio import imread
 import matplotlib.pyplot as plt
 
-from multiview_mapping_toolkit.constants import NULL_TEXTURE_INT_VALUE
+from multiview_mapping_toolkit.constants import (
+    NULL_TEXTURE_INT_VALUE,
+    TEN_CLASS_VIS_KWARGS,
+)
 
 
-def visualize_renders(render_folder, image_folder, n_samples=5, render_extension="png"):
-    rendered_files = np.random.choice(
-        list(Path(render_folder).rglob(f"*.{render_extension}")), n_samples
-    )
+def show_segmentation_labels(
+    label_folder,
+    image_folder,
+    null_label=NULL_TEXTURE_INT_VALUE,
+    imshow_kwargs=TEN_CLASS_VIS_KWARGS,
+    num_show=10,
+    label_suffix=".png",
+    image_suffix=".JPG",
+):
+    rendered_files = list(Path(label_folder).rglob("*" + label_suffix))
+    np.random.shuffle(rendered_files)
 
-    for rendered_file in rendered_files:
+    for rendered_file in rendered_files[:num_show]:
         image_file = Path(
-            image_folder, rendered_file.relative_to(render_folder)
-        ).with_suffix(".JPG")
+            image_folder, rendered_file.relative_to(label_folder)
+        ).with_suffix(image_suffix)
+
         image = imread(image_file)
         render = imread(rendered_file).astype(float)
-        render[render == NULL_TEXTURE_INT_VALUE] = np.nan
-        f, ax = plt.subplots(1, 2)
+        render[render == null_label] = np.nan
+        _, ax = plt.subplots(1, 2)
         ax[0].imshow(image)
-        ax[1].imshow(render, vmin=0, vmax=9, cmap="tab10", interpolation="none")
+        ax[1].imshow(
+            render,
+            interpolation="none",
+            cmap=imshow_kwargs["cmap"],
+            vmin=imshow_kwargs["clim"][0],
+            vmax=imshow_kwargs["clim"][1],
+        )
         plt.show()
         plt.close()
