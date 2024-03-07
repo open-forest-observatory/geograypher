2d6654eb0bcb515ad2a943d679745ed7b5d905b6
Ruprabhu25 <ruprabhu25@gmail.com>
2024-03-07 18:46:32 +0000

    Remove lookup segmentor from __init__ to avoid circular import

--- multiview_mapping_toolkit/segmentation/__init__.py
+++ multiview_mapping_toolkit/segmentation/__init__.py
@@ -1,4 +1,3 @@
-from multiview_mapping_toolkit.segmentation.derived_segmentors import LookUpSegmentor
 from multiview_mapping_toolkit.segmentation.ortho_segmentor import (
     assemble_tiled_predictions,
     write_chips,
