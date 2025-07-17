c7460f1c53e51c362958032838df877968277b20
Eric Schneider <franz.eric.schneider@gmail.com>
2025-07-16 20:05:22 -0500

    Vectorize ray intersections

--- tests/test_numeric.py
+++ tests/test_numeric.py
@@ -1,129 +1,163 @@
+import json
+import tempfile
+from pathlib import Path
+
 import numpy as np
 import pytest
 
 from geograypher.utils.numeric import (
-    compute_approximate_ray_intersection,
+    compute_approximate_ray_intersections,
     intersection_average,
 )
 
 
 class TestComputeApproximateRayIntersection:
 
+    # These are all formatted as
+    # [a0[0], a0[1], ...] - (N, 3) - all the starting points of the A vectors
+    # [a1[0], a1[1], ...] - (N, 3) - all the ending points of the A vectors
+    # [b0[0], b0[1], ...] - (N, 3) - all the starting points of the B vectors
+    # [b1[0], b1[1], ...] - (N, 3) - all the ending points of the B vectors
+    # bool - whether to clamp or calculate for infinite rays
+    # [A[0] to B[0], A[1] to B[0], ...] - (N, N, 3) - expected "closest point" on the A vectors
+    # [A[0] to B[1], A[1] to B[1], ...]
+    # [...]
+    # [A[0] to B[0], A[1] to B[0], ...] - (N, N, 3) - expected "closest point" on the B vectors
+    # [A[0] to B[1], A[1] to B[1], ...]
+    # [...]
+    # [A[0] to B[0], A[1] to B[0], ...] - (N, N) - expected distance between closest points
+    # [A[0] to B[1], A[1] to B[1], ...]
+    # [...]
+    # bool - whether the lines are parallel (ignore closest point logic)
     @pytest.mark.parametrize(
-        "a0, a1, b0, b1, clamp, exp_pA, exp_pB, exp_dist",
+        "a0, a1, b0, b1, clamp, exp_pA, exp_pB, exp_dist, parallel",
         (
-            # Test simple intersection
+            # Test simple intersections
             (
-                [0, 0, 0],
-                [2, 0, 0],
-                [1, 1, 0],
-                [1, -1, 0],
+                [[0, 0, 0], [0, 0, 0]],
+                [[2, 0, 0], [2, 2, 0]],
+                [[1, 1, 0], [2, 0, 0]],
+                [[1, -1, 0], [1, 1, 0]],
                 False,
-                [1, 0, 0],
-                [1, 0, 0],
-                0,
+                [[[1, 0, 0], [2, 0, 0]], [[1, 1, 0], [1, 1, 0]]],
+                [[[1, 0, 0], [2, 0, 0]], [[1, 1, 0], [1, 1, 0]]],
+                [[0, 0], [0, 0]],
+                False,
             ),
-            # Test simple miss case
+            # Test simple miss cases, infinite rays (no clamping)
             (
-                [0, 0, 0],
-                [1, 0, 0],
-                [0, 1, 1],
-                [0, 0, 1],
+                [[0, 0, 0], [0, 1, -1]],
+                [[1, 0, 0], [1, 1, -1]],
+                [[0, 1, 1], [0, -4, 2]],
+                [[0, 0, 1], [2, -2, 2]],
                 False,
-                [0, 0, 0],
-                [0, 0, 1],
-                1,
+                [[[0, 0, 0], [4, 0, 0]], [[0, 1, -1], [5, 1, -1]]],
+                [[[0, 0, 1], [4, 0, 2]], [[0, 1, 1], [5, 1, 2]]],
+                [[1, 2], [2, 3]],
+                False,
             ),
             # One much bigger than the other, no clamp
             (
-                [-10, 0, 0],
-                [10, 0, 0],
-                [0, 1, 0],
-                [1, 2, 0],
+                [[-10, 0, 0]],
+                [[10, 0, 0]],
+                [[0, 1, 0]],
+                [[1, 2, 0]],
                 False,
-                [-1, 0, 0],
-                [-1, 0, 0],
-                0,
+                [[[-1, 0, 0]]],
+                [[[-1, 0, 0]]],
+                [[0]],
+                False,
             ),
             # One much bigger than the other, clamp
             (
-                [-10, 0, 0],
-                [10, 0, 0],
-                [0, 1, 0],
-                [1, 2, 0],
+                [[-10, 0, 0], [-10, 0, 1]],
+                [[10, 0, 0], [10, 0, 1]],
+                [[0, 1, 0], [0, 1, 2]],
+                [[1, 2, 0], [0, 1, 1]],
                 True,
-                [0, 0, 0],
-                [0, 1, 0],
-                1,
+                [[[0, 0, 0], [0, 0, 0]], [[0, 0, 1], [0, 0, 1]]],
+                [[[0, 1, 0], [0, 1, 1]], [[0, 1, 0], [0, 1, 1]]],
+                [[1, np.sqrt(2)], [np.sqrt(2), 1]],
+                False,
             ),
             # Somewhat aligned, clamp
             (
-                [1, 0, 0],
-                [2, -4, 0],
-                [-1, 0, 0],
-                [-2, 8, 0],
+                [[1, 0, 0], [0, 0, 0]],
+                [[2, -4, 0], [1, 10, 0]],
+                [[-1, 0, 0], [3, -5, 0]],
+                [[-2, 8, 0], [3, -10, 0]],
                 True,
-                [1, 0, 0],
-                [-1, 0, 0],
-                2,
-            ),
-            # One at the endpoint, one along the vector, clamp
-            (
-                [2, 2, 0],
-                [3, 3, 0],
-                [1, 0, 0],
-                [3, 0, 0],
-                True,
-                [2, 2, 0],
-                [2, 0, 0],
-                2,
+                [[[1, 0, 0], [2, -4, 0]], [[0, 0, 0], [0, 0, 0]]],
+                [[[-1, 0, 0], [3, -5, 0]], [[-1, 0, 0], [3, -5, 0]]],
+                [[2, np.sqrt(2)], [1, np.sqrt(34)]],
+                False,
             ),
             # Parallel, with overlap
             (
-                [0, 0, 0],
-                [1, 0, 0],
-                [0, 1, 0],
-                [1, 1, 0],
+                [[0, 0, 0], [0, -1, 0]],
+                [[1, 0, 0], [1, -1, 0]],
+                [[0, 1, 0], [0.4, 2, 0], [-1, 3, 0], [0.9, 4, 0]],
+                [[1, 1, 0], [0.6, 2, 0], [0.1, 3, 0], [2, 4, 0]],
                 True,
                 None,
                 None,
-                1,
+                [[1, 2, 3, 4], [2, 3, 4, 5]],
+                True,
             ),
             # Parallel, with no overlap
             (
-                [0, 0, 0],
-                [1, 0, 0],
-                [4, 4, 0],
-                [5, 4, 0],
+                [[0, 0, 0]],
+                [[1, 0, 0]],
+                [[4, 4, 0]],
+                [[5, 4, 0]],
                 True,
-                [1, 0, 0],
-                [4, 4, 0],
-                5,
+                None,
+                None,
+                [[5]],
+                True,
             ),
         ),
     )
-    def test_basic(self, a0, a1, b0, b1, clamp, exp_pA, exp_pB, exp_dist):
+    def test_basic(self, a0, a1, b0, b1, clamp, exp_pA, exp_pB, exp_dist, parallel):
         """Test a variety of ray intersections."""
 
         # These tests should work regardless of orientation
-        for A0, A1 in [(a0, a1), (a1, a0)]:
-            for B0, B1 in [(b0, b1), (b1, b0)]:
-                pA, pB, dist = compute_approximate_ray_intersection(
-                    np.array(A0),
-                    np.array(A1),
-                    np.array(B0),
-                    np.array(B1),
-                    clamp=clamp,
-                )
-                if exp_pA is None:
-                    assert pA is None
-                else:
-                    assert np.allclose(pA, exp_pA)
-                if exp_pB is None:
-                    assert pB is None
-                else:
-                    assert np.allclose(pB, exp_pB)
-                assert np.isclose(dist, exp_dist)
+        for A in [(a0, a1), (a1, a0)]:
+            for B in [(b0, b1), (b1, b0)]:
+                # We should also be able to swap A and B
+                for (c0, c1), (d0, d1), exp_pC, exp_pD, mirror in [
+                    (A, B, exp_pA, exp_pB, False),
+                    (B, A, exp_pB, exp_pA, True),
+                ]:
+                    pA, pB, dist = compute_approximate_ray_intersections(
+                        a0=np.array(c0),
+                        a1=np.array(c1),
+                        b0=np.array(d0),
+                        b1=np.array(d1),
+                        clamp=clamp,
+                    )
+
+                    # If the lines are parallel the "closest point" gets murky,
+                    # just test the distance
+                    if not parallel:
+                        if exp_pC is None:
+                            assert pA is None
+                        else:
+                            if mirror:
+                                exp_pC = np.swapaxes(exp_pC, 0, 1)
+                            assert np.allclose(pA, exp_pC)
+                        if exp_pD is None:
+                            assert pB is None
+                        else:
+                            if mirror:
+                                exp_pD = np.swapaxes(exp_pD, 0, 1)
+                            assert np.allclose(pB, exp_pD)
+
+                    if mirror:
+                        test_dist = np.array(exp_dist).T
+                    else:
+                        test_dist = exp_dist
+                    assert np.allclose(dist, test_dist)
 
 
 class TestIntersectionAverage:
@@ -179,7 +213,5 @@ class TestIntersectionAverage:
                 [1, 1, 0],
             ]
         )
-        # Should fall back to average of all endpoints
-        all_points = np.concatenate([starts, ends], axis=0)
-        expected = np.mean(all_points, axis=0)
-        assert np.allclose(intersection_average(starts, ends), expected)
+        # Parallel behavior is to snap to a0 for lack of something better to do
+        assert np.allclose(intersection_average(starts, ends), [0, 0.5, 0])
