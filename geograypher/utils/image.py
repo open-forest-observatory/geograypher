Clean up sub-pixel issues and fix rotation conventions

geograypher/utils/image.py
@@ -49,7 +49,7 @@ def rotate_by_roll_pitch_yaw(
     # then applying the rotation, then converting back into the camera frame.
     # TODO determine why we're using the transpose rotation matrix
     rotation_matrix_in_cam_frame = (
-        perumutation_matrix.T @ rotation_matrix.T @ perumutation_matrix
+        perumutation_matrix.T @ rotation_matrix @ perumutation_matrix
     )
 
     return rotation_matrix_in_cam_frame
@@ -108,11 +108,17 @@ def perspective_from_equirectangular(
     # Compute the aspect ratio of the requested image dimensions
     aspect_ratio = out_h / out_w
 
+    # Compute the distance from the center of the image to the edge in the x dimension
+    x_dist = np.tan(fov / 2)
+    y_dist = x_dist * aspect_ratio
+
+    # Compute the size of each pixel to center the rays within the pixel rather than using the
+    # top-left corner
+    pixel_width = (2 * x_dist) / out_w
+
     # Create homogenous image coordinates for the output image
-    x = np.linspace(-np.tan(fov / 2), np.tan(fov / 2), out_w)
-    y = np.linspace(
-        -np.tan(fov / 2) * aspect_ratio, np.tan(fov / 2) * aspect_ratio, out_h
-    )
+    x = np.arange(-x_dist + pixel_width / 2, x_dist, pixel_width)
+    y = np.arange(-y_dist + pixel_width / 2, y_dist, pixel_width)
 
     xv, yv = np.meshgrid(
         x, -y
@@ -130,9 +136,7 @@ def perspective_from_equirectangular(
     # Rotate the pixel directions by the rotation matrix
     # The strange convention here is to deal with the fact that pixel_directions is (w, h, 3)
     # so this allows the dimensions to align for the matrix multiplication.
-    # The permutation matrix can be thought of first converting into the conventional RPY frame,
-    # then applying the rotation, then converting back into the camera frame.
-    pixel_directions = pixel_directions @ rotation_matrix
+    pixel_directions = pixel_directions @ rotation_matrix.T
 
     # Convert 3D directions to spherical coordinates
     # horizontal angle

tests/test_images.py
@@ -41,11 +41,12 @@ def test_equi_to_perspective(yaw_deg, pitch_deg, roll_deg):
     warp_order = 0
 
     # Create a test equirectangular image where the pixel values are the (pitch, yaw) coordinates,
-    # Defined using the center of the image as (0,0)
+    # Defined using the center of the image as (0,0). Note that there is a half pixel offset so
+    # that the center of the pixel corresponds to the angle.
 
     # Negative from image convention
-    i_samples = np.arange(90, -90, -1)
-    j_samples = np.arange(-180, 180, 1)
+    i_samples = np.arange(90 - 0.5, -90, -1)
+    j_samples = np.arange(-180 + 0.5, 180, 1)
 
     ij = np.meshgrid(i_samples, j_samples, indexing="ij")
     img = np.stack(
@@ -69,14 +70,9 @@ def test_equi_to_perspective(yaw_deg, pitch_deg, roll_deg):
     # Re-scale back to original pixel values
     sample = sample * 360.0 - 180.0
 
-    # Check the four pixels at the center of each side to see if the value is what was expected
-    # assert np.allclose(sample[0, 45, :2], expected[0], atol=1)
-    # assert np.allclose(sample[45, 90, :2], expected[1], atol=1)
-    # assert np.allclose(sample[90, 45, :2], expected[2], atol=1)
-    # assert np.allclose(sample[45, 0, :2], expected[3], atol=1)
-
     xyz_sample = convert_py_to_xyz(sample[45, 45, :])
     xyz_input = convert_py_to_xyz([pitch_deg, yaw_deg])
+    # Print out the difference for debugging
     print(
         np.array(xyz_sample) - np.array(xyz_input),
         sample[45, 45, :],
@@ -85,7 +81,7 @@ def test_equi_to_perspective(yaw_deg, pitch_deg, roll_deg):
     assert np.allclose(
         xyz_sample,
         xyz_input,
-        atol=0.02,  # TODO decrease this tolerance
+        atol=0.01,
     )
 
     ## For debugging, uncomment these